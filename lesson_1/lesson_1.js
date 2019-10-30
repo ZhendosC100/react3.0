02cf948746429d7a76b03755778ebf7479a5796a
Yauhen <zhendos@tut.by>
2019-10-30 22:02:46 +0100

    some code improvement

--- lesson_1/lesson_1.js
+++ lesson_1/lesson_1.js
@@ -1,5 +1,5 @@
 
-var employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+let employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
 
 let employersNames = employers.filter(name => name.length > 0).map(item => item.toLocaleLowerCase().trim());
 
