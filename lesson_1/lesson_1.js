cc98ae8629df471b46557cff3b26d996e889fb18
Yauhen <zhendos@tut.by>
2019-10-30 09:12:47 +0100

    some code improvement

--- lesson_1/lesson_1.js
+++ lesson_1/lesson_1.js
@@ -14,16 +14,16 @@ let {cash, eu, rus} = sponsors;
 let calcCash = (...own) => {
     let total = own[1].reduce((firstVal, lastVal) => firstVal + lastVal, own[0]);
     return total;
-}
+};
 
 
 let money = calcCash(null, cash);
 
-function makeBusiness(owner, director = 'Victor', cash, emp){
+let makeBusiness = (owner, director = 'Victor', cash, emp) => {
     let sumSponsors = [...eu, 'unexpected sponsor'];
 
     console.log(`We have a buisness. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our emplouers: ${emp} \n And we have a sponsors:\n ${null, sumSponsors} \n Note. Be careful with ${eu[0]}. It's a huge risk.`);
-}
+};
 
 
 makeBusiness.apply(null, ['Sam', null, money, employersNames]);
