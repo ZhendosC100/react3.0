cc708b4b3ce64475d4deb22351c291daac69697b
Yauhen <zhendos@tut.by>
2019-11-05 22:48:03 +0100

    lesson 4 finished

--- react_twitter/src/components/post-add-form/post-add-form.js
+++ react_twitter/src/components/post-add-form/post-add-form.js
@@ -6,14 +6,14 @@ const PostAddForm = () => {
         <form className="bottom-panel d-flex">
             <input
                 type="text"
-                placeholder="What about think you?"
+                placeholder="What are you thinking about?"
                 className="form-control new-post-label"
             />
             <button
                 type="submit"
                 className="btn btn-outline-secondary"
             >
-            Добавить</button>
+            Add</button>
         </form>
     )
 }
--- react_twitter/src/components/post-list/post-list.js
+++ react_twitter/src/components/post-list/post-list.js
@@ -2,12 +2,30 @@ import React from 'react';
 import './post-list.css';
 import PostListItem from '../post-list-item';
 
-const PostList = ()=> {
+const PostList = ({posts})=> {
+
+    console.log(posts);
+
+    const postsFilt = posts.filter(item => typeof(item) === 'object');
+    
+    const elements = postsFilt.map((item) => {
+        const {id, ...itemProps} = item;
+            return (
+                <li key={id} className='list-group-item'>
+                    <PostListItem {...itemProps}
+    
+                        // label={item.label}
+                        // important={item.important}
+                    />
+                </li>
+            )
+
+        
+    });
+
     return(
         <ul className="app-list list-group">
-            <PostListItem/>
-            <PostListItem/>
-            <PostListItem/>
+            {elements}
         </ul>
     )
 }
